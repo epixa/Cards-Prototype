8c03d5dbf557c26d9cb12b2222b153e4423f766e
Court Ewing <court@epixa.com>
2011-06-01 19:06:17 -0400

    Fixed js bug with ajax error handling; added appropriate ignores for data and logs

--- prototype/public/js/global.js
+++ prototype/public/js/global.js
@@ -1,5 +1,5 @@
 (function($){
-    $.bind('ajax.error', function(data){
+    $(document).bind('ajax.error', function(event, data){
         alert('An error has occurred.  Check your javascript log for details.');
         console.log(data);
     });
