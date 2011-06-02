8824e4e9094c696685d5b57f2663bab64fc88f68
Court Ewing <court@epixa.com>
2011-06-02 15:52:38 -0400

    Added player tracking

--- prototype/src/actions/json/start-game.php
+++ prototype/src/actions/json/start-game.php
@@ -1,10 +1,10 @@
 <?php
 
-$game = array(
-    'name' => 'Up and Down the River',
-    'code' => 'river',
-    'players' => array()
-);
+$game = new stdClass();
+$game->name = 'Up and Down the River';
+$game->code = 'river';
+$game->totalPlayers = 0;
+$game->players = new stdClass();
 
 $result = file_put_contents($gameFile, json_encode($game));
 if ($result === false) {
