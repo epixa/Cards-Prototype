a2c41cff96253301d4f0177a8675ed6a409179a5
Court Ewing <court@epixa.com>
2011-06-02 19:35:56 -0400

    Removed unused javascript

--- prototype/public/js/game.js
+++ prototype/public/js/game.js
@@ -1,41 +1,6 @@
-window.game = window.game | {};
+window.game = window.game || {};
 
 (function($){
-    game = {
-        decks : {
-            'standard' : {
-                suits : [
-                    'C', 'D', 'H', 'S'
-                ],
-                values : [
-                    2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'
-                ]
-            }
-        },
-
-        roomFactory : function(roomContainer, tableContainer, playersContainer){
-            var room = new game.Room(roomContainer);
-
-            room.setTable(new game.Table(tableContainer));
-
-            playersContainer.each(function(){
-                room.addPlayer(new game.Player($(this)));
-            });
-
-            return room;
-        },
-
-        cardFactory : function(value, suit)
-        {
-            var container = $('<span/>');
-
-            container.attr('id', 'card-' + suit + '-' + value);
-            container.html(suit + '-' + value);
-
-            return new game.Card(container, suit, value);
-        }
-    };
-
     game.Room = function(url) {
         var self = this;
 
@@ -81,16 +46,6 @@ window.game = window.game | {};
         }
     };
 
-    game.Table =  function(container) {
-        var self = this;
-
-        self.cards = {};
-
-        self.addCard = function(card){
-            self.cards[card.id] = card;
-        };
-    };
-
     game.Player = function(data, container) {
         var self = this;
 
@@ -112,8 +67,6 @@ window.game = window.game | {};
             self.container.attr('id', 'player-' + self.data.id);
             self.container.html(self.data.name);
 
-            console.log(self.data.id + ' ' + self.data.lastActivity);
-
             self.container.removeClass('inactive');
             if (!self.isActive()) {
                 self.container.addClass('inactive');
@@ -125,31 +78,5 @@ window.game = window.game | {};
         };
 
         self.populate(data);
-    };
-
-    game.Card = function(container, suit, value) {
-        var self = this;
-
-        self.suit = suit;
-        self.value = value;
-        self.hidden = false;
-
-        self.flip = function(){
-            if (self.hidden) {
-                self.show();
-            } else {
-                self.hide();
-            }
-        };
-
-        self.show = function(){
-            self.hidden = false;
-            container.show();
-        };
-
-        self.hide = function(){
-            self.hidden = true;
-            container.hide();
-        };
     };
 })(jQuery);
